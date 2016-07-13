43d230a760b389b57f17866b79b3dcf09a1de5c9
RossMStewart <rossste@hotmail.co.uk>
2016-07-13 12:15:22 +0100

    4th Commit
    Added functionality of the autoloader.php file.

--- Code/Core/autoloader.php
+++ Code/Core/autoloader.php
@@ -3,5 +3,14 @@ class Auotloader
 {
 	public static function loader($class)
 	{
+		$filename = strtolower($class) . '.php';
+		$file ='code/core/' . $filename;
+		if (!file_exists($file))
+		{
+			return false;
+		}
+		include $file;
 	}
 }
+
+?>
