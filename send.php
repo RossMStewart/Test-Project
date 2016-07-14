9d1daf2660dadf7dd674607e27e48ff7558be53a
RossMStewart <rossste@hotmail.co.uk>
2016-07-14 12:34:05 +0100

    8th Commit
    Added line breaks to the send.php file.

--- send.php
+++ send.php
@@ -1,13 +1,13 @@
 <?php
-
 include "Code/bootstrap.php";
 
 $mailer = new Mailer();
 
-$mailer->addRecipient('me@example.com');
-$mailer->addRecipient('foo@example.com');
+$mailer->addRecipient('me@example.com<br>');
+$mailer->addRecipient('foo@example.com<br>');
 $mailer->addRecipient('bar@example.com');
 
 $mailer->send('This message has been sent to 3 recipients');
 
+    
 ?>
